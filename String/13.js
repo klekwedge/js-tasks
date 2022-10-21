e748cade4ca3d69a286f2499b09841c9ad7574e0
klekkwedge <antonov.andrey.10101@gmail.com>
2022-10-21 20:32:59 +0300

    feat: add solution for string number 13 problem

---
+++ String/13.js
@@ -0,0 +1,10 @@
+// Повторить строку n раз
+// Напишите функцию repeatStr(str, n),
+// которая вовращает строку повторяемую определённое
+// количество раз.
+
+function repeatStr(str, n) {
+  return str.repeat(n);
+}
+
+console.log(repeatStr('Hello', 3)); // HelloHelloHello
