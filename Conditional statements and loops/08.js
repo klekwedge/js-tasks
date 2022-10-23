388bb811063f8df64d9bd135e42cb85b3ea6f639
klekkwedge <antonov.andrey.10101@gmail.com>
2022-10-23 18:03:40 +0300

    feat: add other solution for terms number 8 problem

--- Conditional statements and loops/08.js
+++ Conditional statements and loops/08.js
@@ -5,21 +5,57 @@
 // этот процесс заканчивается на 1, являются счастливыми числами.".
 // Напишите программу, чтобы найти и распечатать первые 5 счастливых чисел.
 
-for (let i = 1; i <= 100; i++) {
-  let el = String(i);
-  let sumSquares = 0;
-  let count = 0;
+// for (let i = 1; i <= 100; i++) {
+//   let el = String(i);
+//   let sumSquares = 0;
+//   let count = 0;
 
-  while (sumSquares !== 1 && count < 100) {
-    sumSquares = 0;
-    for (let i = 0; i < el.length; i++) {
-      sumSquares += +el[i] * +el[i];
+//   while (sumSquares !== 1 && count < 100) {
+//     sumSquares = 0;
+//     for (let i = 0; i < el.length; i++) {
+//       sumSquares += +el[i] * +el[i];
+//     }
+//     count++;
+//     el = String(sumSquares);
+//   }
+
+//   if (sumSquares === 1) {
+//     console.log('Счастливое число', i);
+//   }
+// }
+
+function happy_number(num) {
+  let m, n;
+  let c = [];
+
+  while (num !== 1 && c[num] !== true) {
+    c[num] = true;
+    m = 0;
+    while (num > 0) {
+      n = num % 10;
+      m += n * n;
+      num = (num - n) / 10;
     }
-    count++;
-    el = String(sumSquares);
+    num = m;
+    console.log('num', num);
+    console.log('c[num]', c[num]);
   }
-
-  if (sumSquares === 1) {
-    console.log('Счастливое число', i);
-  }
+  console.log('!!!!!');
+  console.log(c);
+  console.log('========================');
+  return num === 1;
 }
+
+let count = 3;
+let num = 1;
+let f5 = '';
+
+while (count-- > 0) {
+  while (!happy_number(num)) {
+    num++;
+  }
+  f5 += num + ', ';
+
+  num++;
+}
+console.log('Первые 5 счастливых чисел: ' + f5);
