b33ec753a9917661f5055486f7e6eb578d26c4db
klekkwedge <antonov.andrey.10101@gmail.com>
2022-10-23 11:53:41 +0300

    feat: add solution for array number 7 problem

---
+++ Array/07.js
@@ -0,0 +1,15 @@
+// Отобразить элементы в случайном порядоке
+// Используя метод sort перепишите предсталенный ниже код,
+// который с помощью цикла for случайно изменяет порядок
+// расстановки элементов массива.
+
+function shuffle(arr) {
+  for (let i = arr.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+  return arr
+}
+
+const array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+console.log(shuffle(array)); // [5, 4, 7, 6, 0, 3, 1, 2, 9, 8]
