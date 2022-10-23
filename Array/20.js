e1fa338c6e90d9870f134ebf2c49997b80afa292
klekkwedge <antonov.andrey.10101@gmail.com>
2022-10-23 14:50:09 +0300

    feat: add solution for array number 20 problem

---
+++ Array/20.js
@@ -0,0 +1,17 @@
+// Сортировка массива объектов
+// Напишите код, который отсортирует массив объектов litmir
+// по значению свойства title.
+
+const litmir = [
+  { author: 'Хэленка', title: 'Улетела сказка' },
+  { author: 'Коул Кресли', title: 'Восстание Аркан' },
+  { author: 'Райчел Мид', title: 'Золотая лилия' },
+];
+
+function compare(a, b) {
+  if (a.title < b.title) return -1;
+  if (a.title > b.title) return 1;
+  return 0;
+}
+
+console.log(litmir.sort(compare).map((item) => item.title)); // [ 'Восстание Аркан', 'Золотая лилия', 'Улетела сказка' ]
