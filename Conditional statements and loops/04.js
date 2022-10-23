207837ccef6901e6b3c10b26764eded3661df3e3
klekkwedge <antonov.andrey.10101@gmail.com>
2022-10-23 16:47:36 +0300

    feat: add solution for terms number 4 problem

---
+++ Conditional statements and loops/04.js
@@ -0,0 +1,22 @@
+// Отсортируйте три числа по убыванию
+// Используйте условный оператор для сортировки трех чисел по убыванию.
+
+const a = -6;
+const b = -3;
+const c = -6;
+const d = 0;
+const e = -1;
+
+if (a > b && a > c && a > d && a > e) {
+  console.log(a);
+} else if (b > a && b > c && b > d && b > e) {
+  console.log(b);
+} else if (c > a && c > b && c > d && c > e) {
+  console.log(c);
+} else if (d > a && d > b && d > c && d > e) {
+  console.log(d);
+} else {
+  console.log(e);
+}
+
+// 0
