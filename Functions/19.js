a1d201535112ac3ed457640d524ffc6497a54b9d
klekkwedge <antonov.andrey.10101@gmail.com>
2022-10-26 16:02:38 +0300

    feat: add solution problem with function number 19

---
+++ Functions/19.js
@@ -0,0 +1,10 @@
+//  Подсчитать количество вхождений указанной буквы в строке
+// Напишите функцию, которая принимает два аргумента: строку и букву.
+// Функция должна подсчитывать количество вхождений указанной буквы в строке.
+
+function charCount(str, letter) {
+  const pattern = new RegExp(letter, 'gi');
+  return str.match(pattern).length;
+}
+
+console.log(charCount('hello world', 'o')); // 2
