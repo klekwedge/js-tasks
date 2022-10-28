806b02e1d629b0ac5f55631f2a38c5e46ae715b8
klekkwedge <antonov.andrey.10101@gmail.com>
2022-10-28 19:49:50 +0300

    feat: add solution problem with date number 45

---
+++ Date and time/45.js
@@ -0,0 +1,11 @@
+// Получить разницу во времени в часах между двумя датами
+// Напишите функцию, чтобы получить разницу во времени в часах между двумя датами.
+
+function diffMinutes(dt1, dt2) {
+  return Math.abs(Math.round((dt1 - dt2) / 1000 / 60 / 60));
+}
+
+const dt1 = new Date("October 15, 2020 11:11:00");
+const dt2 = new Date("October 15, 2020 17:11:00");
+
+console.log(diffMinutes(dt1, dt2)); // 6
