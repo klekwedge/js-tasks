40dece633a9545a1158799e3c0a18042cb89bba8
klekkwedge <antonov.andrey.10101@gmail.com>
2022-10-28 19:52:18 +0300

    feat: add solution problem with date number 47

--- Date and time/46.js
+++ Date and time/46.js
@@ -1,11 +1,11 @@
 // Получить разницу во времени в днях между двумя датами
 // Напишите функцию, чтобы получить разницу во времени в днях между двумя датами.
 
-function diffMinutes(dt1, dt2) {
+function diffDays(dt1, dt2) {
   return Math.abs(Math.round((dt1 - dt2) / 1000 / 60 / 60 / 24));
 }
 
 const dt1 = new Date("October 15, 2020 11:11:00");
 const dt2 = new Date("October 19, 2020 17:11:00");
 
-console.log(diffMinutes(dt1, dt2)); // 4
+console.log(diffDays(dt1, dt2)); // 4
